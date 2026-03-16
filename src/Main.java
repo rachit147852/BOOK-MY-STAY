0db496e9daf6b989ecb1b90fa28bdefcded1f389
rachit147852 <so3291500@gmail.com>
2026-03-16 14:44:05 +0530

    UC2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        }
+
+import java.util.ArrayList;
+
+class Room {
+    String roomType;
+    int price;
+
+    Room(String roomType, int price) {
+        this.roomType = roomType;
+        this.price = price;
     }
 
+    void display() {
+        System.out.println("Room Type: " + roomType + " | Price: ₹" + price);
+    }
+}
+
+public class RoomInitialization {
+
+    public static void main(String[] args) {
+
+        System.out.println("BOOK MY STAY APP - Room Inventory v3.0\n");
+
+        ArrayList<Room> rooms = new ArrayList<>();
+
+        rooms.add(new Room("Single Room", 2500));
+        rooms.add(new Room("Double Room", 4000));
+        rooms.add(new Room("Suite Room", 7500));
+
+        for (Room r : rooms) {
+            r.display();
+        }
+    }
+}
