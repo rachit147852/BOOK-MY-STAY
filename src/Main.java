6d6d9d672ac1d440c06546016fb90a212036c911
rachit147852 <so3291500@gmail.com>
2026-03-24 13:43:22 +0530

    UC2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,83 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+abstract class Room {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    protected String roomType;
+    protected int beds;
+    protected double size;
+    protected double price;
+
+    public Room(String roomType, int beds, double size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type : " + roomType);
+        System.out.println("Beds      : " + beds);
+        System.out.println("Size      : " + size + " sq.ft");
+        System.out.println("Price     : ₹" + price + " per night");
+    }
+}
+
+
+/* Single Room */
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 200, 2500);
+    }
+}
+
+
+/* Double Room */
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 4000);
+    }
+}
+
+
+/* Suite Room */
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 550, 7500);
+    }
+}
+
+
+/* Main Class for Use Case 2 */
+public class UseCase2RoomInitialization {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("        BOOK MY STAY APP");
+        System.out.println("     Hotel Booking System v2.1");
+        System.out.println("====================================");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        System.out.println("\nAvailable Room Types\n");
+
+        single.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleAvailability);
+        System.out.println("------------------------------------");
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleAvailability);
+        System.out.println("------------------------------------");
+
+        suite.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteAvailability);
+        System.out.println("------------------------------------");
     }
 }
